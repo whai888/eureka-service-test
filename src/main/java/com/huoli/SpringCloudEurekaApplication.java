ff35f01ba3aa6ba62c8c430cb26f87e1e6acea10
wanghai@133.cn <wanghai@133.cn>
2018-08-15 17:00:19 +0800

    init1

---
+++ src/main/java/com/huoli/SpringCloudEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.huoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author whai
+ * @date 2018年4月27日
+ */
+@SpringBootApplication
+public class SpringCloudEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
